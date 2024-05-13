3f8ff36f3d53d6b10b49277ab55325118badb35f
Amelia1110 <ameliasongjiyun@hotmail.com>
2024-05-13 13:08:48 -0400

    decorating

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   const [springExtension, setSpringExtension] = useState<Number>();
@@ -29,7 +27,9 @@ function App() {
   }
 
   return (
-    <div className="grid gap-4 place-content-center h-screen">
+    <div className="grid gap-4 place-content-center h-screen bg-slate-900 text-white">
+      <img className="absolute bottom-0 right-0 h-full w-full" src="space.avif" alt="bongo cat gif"/>
+      <div className="h-full w-full p-10 z-10 bg-opacity-30 rounded-lg bg-white">
       <div className="flex justify-center">
         <h1 className="text-xl font-semibold">Spring Launcher Calculator</h1>
       </div>
@@ -47,13 +47,16 @@ function App() {
           <input required type="number" name="distanceY" className="bg-gray-100 h-8 w-full rounded-md" step="any"/>
         </div>
         <div className="flex gap-2 pt-6">
-          <button type="reset" className="bg-gray-200 rounded-md w-full">Reset</button>
-          <button type="submit" className="bg-blue-600 text-white rounded-md w-full">Submit</button>
+          <button type="reset" className="bg-gray-200 text-black rounded-md w-full">Reset</button>
+          <button type="submit" className="bg-blue-600 rounded-md w-full">Submit</button>
         </div>
       </form>
       <div>
         {springExtension !== undefined ? " Spring Extension: " + parseFloat(springExtension.toString()).toFixed(2) + "cm" : null}
       </div>
+      </div>
+      <img className="absolute h-3/5" src="cat-nyan-cat.gif" alt="nyan cat gif"/>
+      <img className="absolute bottom-0 right-0 h-3/5" src="bongo-cat.gif" alt="bongo cat gif"/>
     </div>
   );
 }
