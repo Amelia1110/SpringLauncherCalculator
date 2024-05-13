d4c4cada32888b5e57bbbba1a36da30c7360a2bb
Amelia1110 <ameliasongjiyun@hotmail.com>
2024-05-13 13:29:30 -0400

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -30,40 +30,40 @@ function App() {
     <div className="grid gap-4 place-content-center h-screen bg-slate-900 text-white">
       <img className="absolute bottom-0 right-0 h-full w-full" src="space.avif" alt="bongo cat gif"/>
       <div className="h-full w-full p-10 z-10 bg-opacity-30 rounded-lg bg-white">
-      <div className="flex justify-center">
-        <h1 className="text-xl font-semibold">Spring Launcher Calculator</h1>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <div className="flex gap-5">
-          <div className="py-2">
-            <p>Spring Constant</p>
-            <input required type="number" name="k" defaultValue="48.9" className="bg-gray-100 p-2 text-black h-8 w-full rounded-md" step="any"/>
+        <div className="flex justify-center">
+          <h1 className="text-xl font-semibold">Spring Launcher Calculator</h1>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="flex gap-5">
+            <div className="py-2">
+              <p>Spring Constant</p>
+              <input required type="number" name="k" defaultValue="48.9" className="bg-gray-100 p-2 text-black h-8 w-full rounded-md" step="any"/>
+            </div>
+            <div className="py-2">
+              <p>Spring Mass (g)</p>
+              <input required type="number" name="m" defaultValue="11.7" className="bg-gray-100 p-2 text-black h-8 w-full rounded-md" step="any"/>
+            </div>
           </div>
           <div className="py-2">
-            <p>Spring Mass (g)</p>
-            <input required type="number" name="m" defaultValue="11.7" className="bg-gray-100 p-2 text-black h-8 w-full rounded-md" step="any"/>
+            <p>Angle</p>
+            <input required type="number" name="angle" className="bg-gray-100 p-2 text-black h-8 w-full rounded-md" step="any"/>
           </div>
+          <div className="py-2">
+            <p>Distance - Horizontal</p>
+            <input required type="number" name="distanceX" className="bg-gray-100 p-2 text-black h-8 w-full rounded-md" step="any"/>
+          </div>
+          <div className="py-2">
+            <p>Distance - Vertical</p>
+            <input required type="number" name="distanceY" className="bg-gray-100 p-2 text-black h-8 w-full rounded-md" step="any"/>
+          </div>
+          <div className="flex gap-2 pt-6">
+            <button type="reset" className="bg-gray-200 text-black rounded-md w-full">Reset</button>
+            <button type="submit" className="bg-blue-600 rounded-md w-full">Submit</button>
+          </div>
+        </form>
+        <div className="p-2 mt-4 bg-black rounded-md">
+          {springExtension !== undefined ? " Spring Extension: " + parseFloat(springExtension.toString()).toFixed(2) + "cm" : "why is there nothing here smh"}
         </div>
-        <div className="py-2">
-          <p>Angle</p>
-          <input required type="number" name="angle" className="bg-gray-100 p-2 text-black h-8 w-full rounded-md" step="any"/>
-        </div>
-        <div className="py-2">
-          <p>Distance - Horizontal</p>
-          <input required type="number" name="distanceX" className="bg-gray-100 p-2 text-black h-8 w-full rounded-md" step="any"/>
-        </div>
-        <div className="py-2">
-          <p>Distance - Vertical</p>
-          <input required type="number" name="distanceY" className="bg-gray-100 p-2 text-black h-8 w-full rounded-md" step="any"/>
-        </div>
-        <div className="flex gap-2 pt-6">
-          <button type="reset" className="bg-gray-200 text-black rounded-md w-full">Reset</button>
-          <button type="submit" className="bg-blue-600 rounded-md w-full">Submit</button>
-        </div>
-      </form>
-      <div className="p-2 mt-4 bg-black rounded-md">
-        {springExtension !== undefined ? " Spring Extension: " + parseFloat(springExtension.toString()).toFixed(2) + "cm" : "why is there nothing here smh"}
-      </div>
       </div>
       <img className="absolute h-3/5" src="cat-nyan-cat.gif" alt="nyan cat gif"/>
       <img className="absolute bottom-0 right-0 h-3/5" src="bongo-cat.gif" alt="bongo cat gif"/>
